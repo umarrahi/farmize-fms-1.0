eadacd97e6cb5d973fd344c42f1ceddd98edd7d6
Umar Rahi <125821878+umarrahi@users.noreply.github.com>
2025-01-13 16:23:00 +0500

    update

--- resources/views/backend/calender.blade.php
+++ resources/views/backend/calender.blade.php
@@ -19,7 +19,7 @@
                 <div
                     class="card-header border-b border-neutral-200 dark:border-neutral-600 bg-white dark:bg-neutral-700 text-end">
                     <button type="button" data-hs-overlay="#hs-slide-up-animation-modal"
-                        class="btn bg-primary text-white gap-2">
+                        class="py-2 px-4 inline-flex justify-center items-center gap-2 border font-medium border-transparent bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm">
                         <span><i class="uil uil-plus"></i></span>
                         <span> Create</span>
                     </button>
@@ -30,16 +30,17 @@
                 </div>
             </div>
 
-            {{-- Modal Add --}}
+            {{-- Modal Box --}}
             <div id="hs-slide-up-animation-modal"
                 class="hs-overlay hidden w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto">
                 <div
                     class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-14 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
                     <div class="flex flex-col bg-white border shadow-sm rounded-xl">
+                        
                         {{-- Modal Header  --}}
                         <div class="flex justify-between items-center py-3 px-4 border-b">
                             <h3 class="font-bold text-gray-800">
-                                Modal title
+                               Add New Task
                             </h3>
                             <button type="button"
                                 class="hs-dropup-toggle inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-md text-gray-500 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-sm"
@@ -53,24 +54,67 @@
                                 </svg>
                             </button>
                         </div>
-                          {{-- Modal Body  --}}
-                        <div class="p-4 overflow-y-auto">
-                            <p class="mt-1 text-gray-400">
-                                This is a wider card with supporting text below as a natural
-                                lead-in
-                                to additional content.
-                            </p>
+
+                        {{-- Modal Body  --}}
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 p-4 overflow-y-auto ">
+                            
+                            <div>
+                                <label
+                                    class="text-gray-800 text-sm font-medium inline-block mb-2">Task Name</label>
+                                <input type="text" name="" id="" value="" placeholder="Enter task" class="form-input">
+                            </div>
+                            <div>
+                                <label class="text-gray-800 text-sm font-medium inline-block mb-2">Assginee</label>
+                                <select id="" name="" required
+                                    class="form-select cursor-pointer">
+                                    <option value="">Not Assign</option>
+                                    <option value="">Assign 1</option>
+                                    <option value="">Assign 2</option>ٖ
+                                </select>
+                            </div>
+                            <div>
+                                <label class="text-gray-800 text-sm font-medium inline-block mb-2">Due Date</label>
+                                <input type="datetime-local" name="" id="" value="" class="form-input">
+                            </div>
+                            <div>
+                                <label class="text-gray-800 text-sm font-medium inline-block mb-2">Category</label>
+                                <select id="" name="" required
+                                    class="form-select cursor-pointer">
+                                    <option value="">No category</option>
+                                    <option value="">Category 1</option>
+                                    <option value="">Category 2</option>ٖ
+                                </select>
+                            </div>
+                            <div>
+                                <label class="text-gray-800 text-sm font-medium inline-block mb-2">Location</label>
+                                <select id="" name="" required
+                                    class="form-select cursor-pointer">
+                                    <option value="">No location</option>
+                                    <option value="">location 1</option>
+                                    <option value="">location 2</option>ٖ
+                                </select>
+                            </div>
+                            <div>
+                                <label class="text-gray-800 text-sm font-medium inline-block mb-2">Priority*</label>
+                                <select id="" name="" required
+                                    class="form-select cursor-pointer">
+                                    <option value="">Select option</option>
+                                    <option value="">Medium</option>
+                                    <option value="">Large</option>ٖ
+                                </select>
+                            </div>
                         </div>
+
                           {{-- Modal Footer  --}}
                         <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t">
                             <button type="button"
-                                class="hs-dropup-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
+                                class="hs-dropup-toggle py-2 px-4 inline-flex justify-center items-center gap-2 border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
                                 data-hs-overlay="#hs-slide-up-animation-modal">
                                 Close
                             </button>
-                            <a class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"
+                            <a class="py-2 px-4 inline-flex justify-center items-center gap-2 border border-transparent font-medium bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"
                                 href="#">
-                                Save changes
+                                Save Changes
                             </a>
                         </div>
                     </div>
